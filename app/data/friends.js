ee7e2206b09a99eee61b69d1e74d47d7c76beb00
chrissydc <bychriscelle@gmail.com>
2018-08-25 16:30:57 -0400

    survey page layout polished

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+var friends = {
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        
+    ]
+}
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,8 @@
+app.get("/", function (req, res) {
+    if (err) throw err;
+
+})
+
+app.get("/survey", function(req, res) {
+    
+})
